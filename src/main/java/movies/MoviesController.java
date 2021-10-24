bcc41dad8f47a7378aa2b2cb9338ce19959165fb
Jekaterina Stasikova <stasikova.ekaterina@gmail.com>
2021-10-24 18:21:04 +0300

    moviecontroller update

--- src/main/java/movies/MoviesController.java
+++ src/main/java/movies/MoviesController.java
@@ -1,11 +1,14 @@
 package movies;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class MoviesController {
+    MoviesRepository moviesRepository = new MoviesRepository();
 
-    public void createMovie(Movies movie){
-
+    public String createMovie(Movies movie){
+        moviesRepository.createMovie(movie);
+        return "Movie created successfully";
     }
     public void updateMovie(Movies movie){
 
