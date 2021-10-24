4f4fc49a8feed5ee0f25090eafeac1a7216ea8b0
Jekaterina Stasikova <stasikova.ekaterina@gmail.com>
2021-10-24 18:26:54 +0300

    controller update 2

--- src/main/java/movies/MoviesController.java
+++ src/main/java/movies/MoviesController.java
@@ -10,13 +10,29 @@ public class MoviesController {
         moviesRepository.createMovie(movie);
         return "Movie created successfully";
     }
-    public void updateMovie(Movies movie){
-
+    public String updateMovie(Movies movie){
+        moviesRepository.updateMovie(movie);
+        return "Movie updated successfully";
     }
     public ArrayList<Movies> getAllMovies(){
-        return new ArrayList<>();
+        return moviesRepository.getAll();
     }
     public Movies findMovie(String name){
-        return new Movies();
+        try {
+            return moviesRepository.getMovieByName(name);
+        } catch (SQLException e) {
+            System.out.println("Cannot retrieve movie from database");
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public Movies findMovie(int id){
+        try {
+            return moviesRepository.getMovie(id);
+        } catch (SQLException e) {
+            System.out.println("Cannot find movie with id: " + id);
+            e.printStackTrace();
+            return null;
+        }
     }
 }
